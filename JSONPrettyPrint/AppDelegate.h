62b074bb5dbce4e0a9303f6895a0d6bd206613b2
Scott Richards <scott.srichards@gmail.com>
2015-07-22 17:58:31 -0700

    Initial Commit

---
+++ JSONPrettyPrint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONPrettyPrint
+//
+//  Created by Scott Richards on 7/22/15.
+//  Copyright (c) 2015 Scott Richards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONPrettyPrint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONPrettyPrint
+//
+//  Created by Scott Richards on 7/22/15.
+//  Copyright (c) 2015 Scott Richards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
